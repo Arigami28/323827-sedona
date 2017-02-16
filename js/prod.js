e85feaf82650a461e2cd64b3f525756c28d826ee
Arigami28 <Arigami28@gmail.com>
2017-02-17 00:10:18 +0300

    Правки

--- js/prod.js
+++ js/prod.js
@@ -1,9 +1,9 @@
-      var form = document.querySelector(".search-hotels_form-search");
-      var btn = document.querySelector(".search-hotels_btn");
-     	form.classList.add("search-hotels_form-search-off");
-      btn.addEventListener("click", function(event) {
-        event.preventDefault();
-        form.classList.add("search-hotels_form-search-on");
-      });
+var form = document.querySelector(".search-hotels_form-search");
+var btn = document.querySelector(".search-hotels_btn");
+form.classList.add("search-hotels_form-search-off");
+btn.addEventListener("click", function(event) {
+event.preventDefault();
+form.classList.add("search-hotels_form-search-on");
+});
 
-    document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1"></' + 'script>')
+document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1"></' + 'script>')
